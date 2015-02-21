d8616a74d6605d2067b0d7e463287efb85ca5624
Eric Skoglund <eric@pagefault.se>
2015-02-21 22:31:49 +0100

    Typo fix

--- pasteit/__init__.py
+++ pasteit/__init__.py
@@ -3,6 +3,6 @@
 __title__     = 'pasteit'
 __version__   = '1.0.0'
 __author__    = 'Eric Skoglund'
-__license__   = 'GPLv3'
+__license__   = 'GPLv2'
 __copyright__ = 'Copyright 2015 Eric Skoglund'
 
