099e1cd69aa52dbf0cebd89125456aa3d25b9a6c
Eric Skoglund <eric@pagefault.se>
2015-02-28 19:16:45 +0100

    Whitespace
    Whitespace

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(name = 'pasteit',
         version = '1.0.1',
-      description = 'Program that allows you to paste files to pastebin like sites',
-      url = 'http://github.com/ErioIO/pasteit',
-      author = 'Eric Skoglund',
-      author_email = 'eric@pagefault.se',
-      license = 'GPLv3',
-      packages = setuptools.find_packages(),
-      install_requires = [
-          'requests',
-      ],
-      entry_points = {
-          'console_scripts' : ['pasteit = pasteit.pasteit:__main']})
+        description = 'Program that allows you to paste files to pastebin like sites',
+        url = 'http://github.com/ErioIO/pasteit',
+        author = 'Eric Skoglund',
+        author_email = 'eric@pagefault.se',
+        license = 'GPLv3',
+        packages = setuptools.find_packages(),
+        install_requires = [
+                'requests',
+        ],
+        entry_points = {
+                'console_scripts' : ['pasteit = pasteit.pasteit:__main']})
